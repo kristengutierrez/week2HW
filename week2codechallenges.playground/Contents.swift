709206896062d87a833752e6925f88755a70823a
kristenkozmary <kristenkozmary@yahoo.com>
2015-08-11 11:12:26 -0700

    yo

--- week2codechallenges.playground/Contents.swift
+++ week2codechallenges.playground/Contents.swift
@@ -6,16 +6,27 @@ var str = "Hello, playground"
 
 ////Monday////
 ////Code Challenge: Write a function that determines how many words there are in a sentence
-func yeah(counting: NSArray) {
-var sentence = "I will take the Ring to Mordor!"
-var counting = sentence.componentsSeparatedByString(" ")
-var result = counting.count
-}
+still working on this
+//func yeah(counting: NSArray) -> (Int) {
+//var sentence = "I will take the Ring to Mordor!"
+//var counting = sentence.componentsSeparatedByString(" ")
+//var result = counting.count
+//  return result
+//}
 
 ////Tuesday////
 ////Code Challenge: Write a function that returns all the odd elements of an array
 
-var iCantEven = ["1","2","3","4","5"]
+//var iCantEven = ["1","2","3","4","5"]
+//
+//
+//for i in iCantEven {
+//  if i % 2 == 0 {
+//    println("even")
+//  } else {
+//    return i
+//  }
+//}
 
 
 
